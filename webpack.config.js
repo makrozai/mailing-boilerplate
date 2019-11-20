c73c51710e5571001a90fd99fa11541bd51971a1
makrozai <marco.condoric@gmail.com>
2019-11-20 11:46:06 -0500

    (#1) chore: add sass loader, css loader and webpack dev server plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
 module.exports= {
   entry:'./src/js/main.js',
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js'
-  }
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
